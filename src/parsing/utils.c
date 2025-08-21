a5ddda4956c8b1ec0a788d004ac17fa609398cec
Jakob Holterhoff <jholterh@student.42berlin.de>
2025-08-21 10:48:22 +0200

    style: utils norminette conform

--- src/parsing/utils.c
+++ src/parsing/utils.c
@@ -6,14 +6,14 @@
 /*   By: jholterh <jholterh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 16:17:04 by jholterh          #+#    #+#             */
-/*   Updated: 2025/07/12 16:34:34 by jholterh         ###   ########.fr       */
+/*   Updated: 2025/08/21 10:47:19 by jholterh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int     print_error(const char *error_msg, const int return_code)
+int	print_error(const char *error_msg, int return_code)
 {
-    printf(COLOR_RED "ERROR: %s\n" COLOR_RESET, error_msg);
-    return (return_code);
+	printf(COLOR_RED "ERROR: %s\n" COLOR_RESET, error_msg);
+	return (return_code);
 }
