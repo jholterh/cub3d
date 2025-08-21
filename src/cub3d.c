a0cd2ac34656fa032a1f45b10c7fc74b5e00e126
Jakob Holterhoff <jholterh@student.42berlin.de>
2025-08-21 11:06:33 +0200

    style: cub3d.c

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,16 +6,15 @@
 /*   By: jholterh <jholterh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 16:14:24 by jholterh          #+#    #+#             */
-/*   Updated: 2025/08/21 11:02:44 by jholterh         ###   ########.fr       */
+/*   Updated: 2025/08/21 11:04:15 by jholterh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int     main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    if (parsing(argc, argv))
-        return (print_error("found", 10));
-    else
-        return (print_error("was not found", 1));
+	if (parsing(argc, argv))
+		return (print_error("found", 10));
+	return (print_error("was not found", 1));
 }
